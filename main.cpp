1b714bc7ad88ba9bc096841d3546524d36b7697c
johnlanni <ztywto@qq.com>
2014-09-27 00:08:36 +0800

    1
    1

---
+++ main.cpp
@@ -0,0 +1,27 @@
+
+#include"process.h"
+
+using namespace std;
+int main()
+{
+	
+	
+	/*process p("1.txt","1.dat",1);//Ñ¹Ëõ
+	map<char,int> fremap;
+	map<char,string> codetable;
+	fremap=p.fremap;
+	codetable=p.codetable;
+	for(map<char,string>::iterator iter = codetable.begin();iter!=codetable.end();++iter){
+		cout<<iter->first<<" "<<iter->second<<" "<<fremap[iter->first]<<endl;
+	}*/
+
+	process p("1.dat","2.txt",0);//½âÑ¹Ëõ
+	/*unsigned char a = 0xfe;
+	unsigned char b = 0x3f;
+	cout<<a;
+	cout<<b;
+	getchar();
+	*/
+	//getchar();
+
+}
